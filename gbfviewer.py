541d94ca49235e8c2741c6e010ef3a400cc1e308
“Estevelag” <estevelaguco@gmail.com>
2021-08-09 22:36:18 -0500

    final

--- gbfviewer.py
+++ gbfviewer.py
@@ -7,11 +7,10 @@ def location(k):
     if k[1].split("=")[0] == "data":
         data=k[1].split("=")[1]
     else:
-        print(k[1].split("=")[1])
         raise Exception('You should specify the data option')
     return data
-print(sys.argv)
-#print(location(sys.argv))
+
+#print(location(sys.argv))#Validation of the last function
 
 
 def verificarPath():
@@ -22,20 +21,22 @@ def verificarPath():
         raise Exception('location is null')
     
 
-#verificarPath()
-#id=acc:NC_0144
+#verificarPath()#Validation of the last function
+
 ###ID functions
 
-def listaArchivos(path):
+def listaArchivos(path):####ask for extension, specifically gbff
     filelist=[]
     with os.scandir(path) as entries:
         for i in entries:
-            filelist.append(i.name)
+            if i.name[len(i.name)-4:len(i.name)]=="gbff":
+                filelist.append(i.name)
     return filelist
 
-#print(listaArchivos(location(sys.argv)))
+#print(listaArchivos(location(sys.argv)))#Validation of the last function
 
-def ID():
+def ID():# Validator that a path is given
+    verificarPath()
     if sys.argv[2].split("=")[0] == "id":
         path = sys.argv[2].split("=")[1]
         if path != "":##verify that is not empty
@@ -45,28 +46,29 @@ def ID():
     else:
         return ''
 
-def findaccesion(files,accesion):
+def findaccesion(files,accesion): 
+    lines = []
+    accfiles=[]
     for i in files:
-        lines = []
-        accfiles=[]
         with open(i) as file:
             lines = file.readlines()
             for line in lines:
                 if re.search(r'\bACCESSION\b', line):
                     acc=line.split("   ")[1]
                     if re.search(r'\b \b', acc):
-                        acc = acc.split(' ')[0]
+                        acc = acc.split(" ")[0]
                     #print(acc,accesion)
                     if acc[0:len(accesion)] == accesion:
                         accfiles.append(file.name)
+                        break
     return accfiles
 #'AY721616'
-#print(findaccesion(listaArchivos(location(sys.argv)),"AY585228"))
+#print(findaccesion(listaArchivos(location(sys.argv)),"AY585228")) #Validation of the last function
 
-def findname(files,name):
+def findname(files,name): ## This is when a name of the organism is given only
+    namefiles=[]
     for i in files:
         lines = []
-        namefiles=[]
         with open(i) as file:
             lines = file.readlines()
             for line in lines:
@@ -77,12 +79,12 @@ def findname(files,name):
                         namefiles.append(file.name)
     return namefiles
 
-#print(findname(listaArchivos(location(sys.argv)),"Human coronavirus OC43"))
+#print(findname(listaArchivos(location(sys.argv)),"Human coronavirus OC43")) #Validation of the last function
 
-def findprot(files,prot):
+def findprot(files,prot):##This function is when a prot is given and not an accesion
+    protfiles=[]
     for i in files:
         lines = []
-        protfiles=[]
         with open(i) as file:
             lines = file.readlines()
             for line in lines:
@@ -94,7 +96,7 @@ def findprot(files,prot):
     return protfiles
 
 
-def queryfiles():##loop through the files to get the math you want
+def queryfiles():##loop through the files to get the match you want to search according to waht to look for
     filestot=[]
     if ID() != '': ##Search for a file with the specific id
         files=listaArchivos(location(sys.argv))## files in wich we are going loop
@@ -102,8 +104,7 @@ def queryfiles():##loop through the files to get the math you want
         accesion=ID().split(':')[1]
         if type== "prot":
             filestot = findprot(files,accesion)
-        elif type == "acc":
-            print(str(accesion))
+        elif type == "acc":   
             filestot = findaccesion(files,str(accesion))# accesion should be here
         elif type == "name":
             filestot = findname(files, accesion)
@@ -113,63 +114,156 @@ def queryfiles():##loop through the files to get the math you want
         filestot=listaArchivos(location(sys.argv))
     return filestot
 
-#print(queryfiles())##files in which to search
+#print(queryfiles())##files in which to search #Validation of the last function
 
-#maybe define each action of each if as aa function
 
-specificfiles=queryfiles()
-if len(sys.argv != 4):
-     raise Exception('Enter valid options')
-else:
-    queryoption=sys.argv[3]#available files, totals, header, dnaseq, proteinlist, proteinseq
-    if queryoption=="files":
-        for i in specificfiles:
-            lines = []
-            namefiles=[]
-            with open(i) as file:
-                lines = file.readlines()
-                m=str(file.name)+':'
-                print(m+"\n"+lines[0])
-    elif queryoption=="totals":
-        for i in specificfiles:
-            lines = []
-            lines2print=[]
-            enter=0
-            with open(i) as file:
-                lines = file.readlines()
-                for line in lines:
-                    if re.search(r'\b##Genome-Annotation-Data-END##\b', line):
-                        enter = 0
-                        break
-                    if enter==1:
-                        lines2print.append(line)
-                    if re.search(r'\b##Genome-Annotation-Data-START##\b', line):
-                        #lines2print.append('\n') maybe to atrt in newline after each file
-                        u=str(file.name)+':'
-                        lines2print.append(u)
-                        enter=1
-        print('\n'.join(map(str,lines2print)))
-    elif queryoption=="header":
-        pass
-    elif queryoption=="dnaseq":
-        pass
-    elif queryoption=="proteinlist":
-        pass
-    elif queryoption[0:10]=="proteinseq":
-        protseq = queryoption.split('=')[1]
 
-print()
-for i in specificfiles:
+####options of query functions
+
+def filesopt(specificfiles):# get only the files and locus
+    for i in specificfiles:
+        lines = []
+        with open(i) as file:
+            lines = file.readlines()
+            m=str(file.name)+':'
+            print(m+"\n"+lines[0])
+
+def totalopt(specificfiles):##Form Data-START to Data-END
     lines = []
-    namefiles=[]
-    with open(i) as file:
-        lines = file.readlines()
-        for line in lines:
-            if re.search(r'\bORGANISM\b', line):
-                acc=line.split("  ")[2]
-                #print(acc,name)
-                if acc[0:len(name)] == name:
-                    namefiles.append(file.name)
+    lines2print=[]
+    for i in specificfiles:
+        enter=0
+        with open(i) as file:
+            lines = file.readlines()
+            linelocus=lines[0]
+            for line in lines:
+                if re.search(r'\bGenome-Assembly-Data-END\b', line):
+                    enter = 0
+                    break## this break the last loop if im doing it right
+                if enter==1:
+                    lines2print.append(line)
+                if re.search(r'\bGenome-Assembly-Data-START\b', line):
+                    #lines2print.append('\n') maybe to atrt in newline after each file
+                    u=str(file.name)+':'
+                    lines2print.append(u)
+                    lines2print.append(linelocus)
+                    enter=1
+    print('\n'.join(map(str,lines2print)))
+
+def headeropt(specificfiles):##LOCUS throgh ORGANISM
+    lines = []
+    lines2print=[]
+    for i in specificfiles:
+        with open(i) as file:
+            lines = file.readlines()
+            enter= 0
+            for line in lines:
+                if re.search(r'\bREFERENCE\b', line):
+                    enter = 0
+                    break## This is done only once per file
+                if enter==1:
+                    lines2print.append(line)
+                if re.search(r'\bLOCUS\b', line):
+                    #lines2print.append('\n') maybe to atrt in newline after each file
+                    u=str(file.name)+":"
+                    lines2print.append(u)
+                    lines2print.append(line)
+                    enter=1
+    print('\n'.join(map(str,lines2print)))
+
+def locusheader(filename):#Print the locus header of a file
+    with open(filename) as file:
+        line = file.readline()
+        print(line)
+
+def dnaseqopt(specificfiles):## Origin to //
+    lines = []
+    lines2print=[]
+    for i in specificfiles:
+        enter=0
+        with open(i) as file:
+            lines = file.readlines()
+            for line in lines:
+                if line[0:2]=="//":
+                    enter = 0
+                if enter==1:
+                    lines2print.append(line)
+                if re.search(r'\bORIGIN\b', line):
+                    u=str(file.name)+':'
+                    print(locusheader(file.name))
+                    lines2print.append(u)
+                    enter=1
+    print('\n'.join(map(str,lines2print)))
 
 
+def protseqopt(specificfiles,protseq):#/protein_id\ and after it /translation ending in CDS 
+    for i in specificfiles:
+        lines = []
+        p=0
+        space='                     '
+        with open(i) as file:
+            print(file.name+",")
+            print(locusheader(file.name))
+            lines = file.readlines()
+            for line in lines:
+                if p==1 and not (line[0:21] == space):
+                    p=0
+                if re.search(r'\bprotein_id\b', line):
+                    id=line.split("=")[1]
+                    if id[1:len(id)-2] == protseq:
+                        p=1
+                if p==1 and re.search(r'\btranslation\b', line):
+                    seq=line.split("=")[1]
+                    print(seq[1:len(seq)-1])
+                if p==1 and not (re.search(r'\btranslation\b', line)):
+                    print(line.split(space)[1])
+                #
+
+def protlistopt(specificfiles):#protein id list and product
+    for i in specificfiles:
+        lines = []
+        p=0
+        with open(i) as file:
+            print(file.name+",")
+            print(locusheader(file.name))
+            lines = file.readlines()
+            for line in lines:
+                if re.search(r'\bproduct\b', line):
+                    product=line.split("=")[1]
+                    p=1
+                if p==1 and (re.search(r'\bprotein_id\b', line)):
+                    id=line.split("=")[1].split("\n")[0]
+                    print(id[1:len(id)-1]+' '+product[1:len(product)-2])
+        
+
+
+def Principalfunction(): # function that does something depending on the option arg
+    specificfiles=queryfiles()
+    if len(sys.argv) != 4:
+        raise Exception('Enter valid options')
+    else:
+        queryoption=sys.argv[3]#available files, totals, header, dnaseq, proteinlist, proteinseq
+        if queryoption=="files":
+            filesopt(specificfiles)
+        elif queryoption=="totals":## Genome annoation or Genome Assembly
+            totalopt(specificfiles)
+        elif queryoption=="header":##LOCUS throgh ORGANISM
+            headeropt(specificfiles)
+        elif queryoption=="dnaseq": ##ORIGIN
+            dnaseqopt(specificfiles)
+        elif queryoption=="proteinlist":#/protein_id\ and after it /product
+            protlistopt(specificfiles)
+        elif queryoption[0:10]=="proteinseq":#/protein_id\ and after it /translation
+            protseq = queryoption.split('=')[1]
+            protseqopt(specificfiles,protseq)
+
+Principalfunction()
+
+#Example commands:
+
+#command python gbfviewer.py data=. id=acc:NW_008703570 totals  
 ###Command to try python gbfviewer.py data=. id=acc:AY585228 files
+###Command to try python gbfviewer.py data=. id=acc:AY585228 header
+##python gbfviewer.py data=. id=acc:AY585228 proteinseq=AAT84351.1  
+##python gbfviewer.py data=. id=acc:AY585228 dnaseq
+##python gbfviewer.py data=. id=acc:AY585228 proteinlist
